515c335eb032da8e8e951ae27d40db0d980aa9e6
Vignesh <vigneshe04@gmail.com>
2023-10-24 22:17:40 +0530

    Tailwind setup complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-3xl text-green-600'>
           Lets start building BingeFlix-GPT!!
         </p>
         <a
