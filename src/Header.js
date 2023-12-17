c2a8dcaf33be2db03784415f4c9e05888252b0a3
Vignesh <vigneshe04@gmail.com>
2023-12-17 22:45:16 +0530

    sign in page completed

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
 
     return (
         <div>
-            <img className='w-56 absolute mt-4 ml-6  bg-black bg-opacity-10' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='logo'/>
+            <img className='w-56 z-10 bg-gradient-to-b from-black absolute mt-4 ml-6  bg-black bg-opacity-10' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt='logo'/>
         </div>
     )
 
